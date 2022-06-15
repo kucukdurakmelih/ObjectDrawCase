cc1b8a0c5b25e176611ae46bf8d73fa7a33c3d3a
douchifer <90257099+douchifer@users.noreply.github.com>
2022-06-15 23:04:18 +0300

    particle added

--- Assets/Scripts/LevelGameObject.cs
+++ Assets/Scripts/LevelGameObject.cs
@@ -7,6 +7,7 @@ public class LevelGameObject : MonoBehaviour
     [SerializeField] private GameObject mainImage;
     [SerializeField] private GameObject firstRevealImage;
     [SerializeField] private GameObject secondRevealImage;
+    [SerializeField] private GameObject particlePrefab;
 
     public void Init()
     {
@@ -17,11 +18,13 @@ public class LevelGameObject : MonoBehaviour
 
     public void LevelPassed()
     {
+        var particle = Instantiate(particlePrefab, firstRevealImage.transform.position, Quaternion.identity);
         firstRevealImage.SetActive(true);
     }
 
     public void ShowHiddenObject()
     {
+        var particle = Instantiate(particlePrefab, secondRevealImage.transform.position, Quaternion.identity);
         secondRevealImage.SetActive(true);
     }
 
